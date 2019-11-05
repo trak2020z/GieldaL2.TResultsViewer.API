3f12347c3069b58a3d19553a4a099514ee80495f
TomaszRzucidlo <rzucidlo.tomasz@gmail.com>
2019-11-05 22:37:01 +0100

    Init project

---
+++ GieldaL2.TResultsViewer.API/Controllers/GraphController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GieldaL2.TResultsViewer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GraphController : ControllerBase
+    {
+    }
+}
---
+++ GieldaL2.TResultsViewer.API/DTOs/GraphDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GieldaL2.TResultsViewer.API.DTOs
+{
+    public class GraphDTO
+    {
+    }
+}
