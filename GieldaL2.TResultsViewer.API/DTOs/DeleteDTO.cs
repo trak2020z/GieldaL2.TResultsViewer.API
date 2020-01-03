638f3c3e054ddce7dcb1a9caf26a5ea34667992b
TomaszRzucidlo <rzucidlo.tomasz@gmail.com>
2020-01-03 18:23:35 +0100

    clear database, data search with hour

---
+++ GieldaL2.TResultsViewer.API/DTOs/DeleteDTO.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GieldaL2.TResultsViewer.API.DTOs
+{
+    public class DeleteDTO
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+    }
+}
